0501b0b223d7e13806503f6afeaa0f8fadf92938
imaginary84 <imaginary84@gmail.com>
2022-10-15 21:23:32 +0900

    EP06 : react기본으로 회원가입 화면을 만들고 axios로 drf API서버를 호출하여 회원가입을 호출한다. Error발생시 react로직 구현, 저장버튼 활성화 조건, 로딩중 여부, 회원가입성공시 로그인 페이지 이동

---
+++ frontend/src/pages/accounts/Signup.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+
+import Axios from "axios";
+
+import { Alert } from "antd";
+
+import { useNavigate } from "react-router-dom";
+
+const apiUrl = "http://localhost:8000/accounts/signup/";
+
+export default function Signup() {
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [fromDisabled, setFormDisabled] = useState(true);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const { username, password } = inputs;
+
+    console.log("onSubmit", username, password);
+
+    setLoading(true);
+
+    setErrors({});
+
+    Axios.post(apiUrl, inputs)
+      .then((response) => {
+        console.log("response :", response);
+        navigate("/accounts/login");
+      })
+      .catch((error) => {
+        // console.log("error :", error);
+        if (error.response) {
+          console.log("error.response :", error.response);
+          setErrors((prev) => {
+            return {
+              ...prev,
+              username: (error.response.data.username || []).join(""),
+              password: (error.response.data.password || []).join(""),
+            };
+          });
+          // debugger;
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  useEffect(() => {
+    // const isDisabled =
+    //   inputs.username.length === 0 || inputs.password.length === 0;
+
+    //   setFormDisabled(isDisabled);
+
+    // 위 주석 내용과 동일 로직이지만. js에 생소한 기능으로 구현함.
+    const isEnabled = Object.values(inputs).every((s) => s.length > 0);
+
+    setFormDisabled(!isEnabled);
+  }, [inputs]);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          ID : <input type="text" name="username" onChange={onChange} />
+          {errors.username && (
+            <Alert message={errors.username} type="error" showIcon />
+          )}
+        </div>
+        <div>
+          PW : <input type="password" name="password" onChange={onChange} />
+          {errors.password && (
+            <Alert message={errors.password} type="error" showIcon />
+          )}
+        </div>
+        <input
+          type="submit"
+          value="회원가입"
+          disabled={loading || fromDisabled}
+        />
+      </form>
+    </div>
+  );
+}
--- frontend/src/pages/accounts/index.js
+++ frontend/src/pages/accounts/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Profile from "./Profile";
 import Login from "./Login";
+import Signup from "./Signup";
 
 function AccountRoutes() {
   return (
     <Routes>
       <Route path={"login"} element={<Login />} />
       <Route path={"profile"} element={<Profile />} />
+      <Route path={"signup"} element={<Signup />} />
     </Routes>
   );
 }
